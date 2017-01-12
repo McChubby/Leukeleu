397179df5d2fd1386d64db748e8c75d981be9f09
J.R. Kok <ruan.kok@student.hu.nl>
2017-01-12 21:06:41 +0100

    Grid staat nu met random kleuren gevuld.
    De opbouw is er van de grid, nu nog de onClick eventlistener die voor
    de ripple zorgt.

--- wrinkle/src/App.js
+++ wrinkle/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 
 const container = {
   margin: '0 10% 0 10%',
-  backgroundcolor: '#4CAF50'
 };
 const gridStyle = {
   // width: '90%',
@@ -13,15 +12,30 @@ const gridStyle = {
   margin: '0 5% 0 5%'
 };
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function WrinkleIt (e){
+  console.log('Wrinkle:',)
+}
+
 function InRowsDrawCols(props){
   return(
-      <td className="block">1</td>
+      <td style={{backgroundColor: getRandomColor()}} onClick={WrinkleIt}>1</td>
   );
 }
 
 function DrawRowsOnGrid(props){
   return(
     <tr>
+      <InRowsDrawCols />
+      <InRowsDrawCols />
       <InRowsDrawCols />
     </tr>
   );
@@ -32,16 +46,87 @@ function DrawGrid(props){
     <table style={gridStyle}>
       <tbody>
         <DrawRowsOnGrid />
+        <DrawRowsOnGrid />
+        <DrawRowsOnGrid />
       </tbody>
     </table>
   );
 }
 
+function Header(props){
+  return(
+    <div className="App-content">
+      <h1>Let the ripple, <code>wrinkle</code>.</h1>
+    </div>
+  );
+}
+
+var Counter = React.createClass({
+
+  propTypes: {},
+
+  getInitialState: function() {
+    return{
+      rowNumber: 1,
+      colNumber: 2,
+    }
+  },
+
+  incrementRow: function (){
+    this.setState({
+      rowNumber: (this.state.rowNumber + 1),
+    });
+  },
+
+  decrementRow: function (){
+    this.setState({
+      rowNumber: (this.state.rowNumber - 1),
+    });
+  },
+
+  incrementCol: function (){
+    this.setState({
+      colNumber: (this.state.colNumber + 1),
+    });
+  },
+
+  decrementCol: function (){
+    this.setState({
+      colNumber: (this.state.colNumber - 1),
+    });
+  },
+
+  render: function (){
+
+    return(
+      <div>
+        <div style={container} className="Counter">
+          <div className="counter-text">Row count</div>
+          <div className="counter-content">  
+            <button className="counter-action decrement" onClick={this.decrementRow}>-</button>
+              <div className="counter-number">{this.state.rowNumber}</div>
+            <button className="counter-action increment" onClick={this.incrementRow}>+</button> 
+          </div>
+        </div>
+        <div style={container} className="Counter">
+          <div className="counter-text">Column count</div>
+          <div className="counter-content">
+            <button className="counter-action decrement" onClick={this.decrementCol}>-</button>
+              <div className="counter-number">{this.state.colNumber}</div>
+            <button className="counter-action increment" onClick={this.incrementCol}>+</button>
+          </div> 
+        </div>
+      </div>
+    );
+  }
+
+});
+
+
 class Wrinkle extends Component {
   render() {
     return(
       <div className="Wrinkle">
-        <h1>Let the ripple, <code>wrinkle</code>.</h1>
         <div style={container} className="Wrinkle-grid">
           <DrawGrid />
         </div>
@@ -60,7 +145,9 @@ class App extends Component {
             <h2>Welcome to React</h2>
           </div>
         </div>
+          <Header />
         <div className="App-content">
+          <Counter />
           <Wrinkle />
         </div>
       </div>
